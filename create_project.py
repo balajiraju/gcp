618384451c0ba444e4f810f9619e069fc6609fc0
root <root@ip-172-31-4-187.ap-southeast-1.compute.internal>
2017-10-09 04:46:48 +0000

    Adding proper instruction for giving project name

--- create_project.py
+++ create_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-projectname = raw_input("Please enter the Unique project name:")
-print ("The project name will be same as projectid")
+print ("The project name will be same as projectid so please enter unique name")
+projectname = raw_input("Please enter the Unique project name:"
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
